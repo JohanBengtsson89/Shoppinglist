6f50dbec6806b18c9376219f7d3a68499182f718
Johan Bengtsson <johanbengtsson89@gmail.com>
2025-02-24 20:34:56 +0100

    Worked onfixing the relationshipbug

---
+++ Backend/src/test/java/com/project/Shoppinglist/Item/ItemServiceTest.java
@@ -0,0 +1,35 @@
+package com.project.Shoppinglist.Item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllItems() {
+
+    }
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
