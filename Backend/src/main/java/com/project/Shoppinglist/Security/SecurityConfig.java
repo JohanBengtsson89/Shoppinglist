04c9ee5b2af8d33372ce5bdb301bedd67e57d452
Johan Bengtsson <johanbengtsson89@gmail.com>
2025-03-01 14:49:03 +0100

    Testing after failure to push

--- Backend/src/main/java/com/project/Shoppinglist/Security/SecurityConfig.java
+++ Backend/src/main/java/com/project/Shoppinglist/Security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -14,6 +16,19 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(new AntPathRequestMatcher("/**"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer () {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+
     }
 
 /*    @Bean
